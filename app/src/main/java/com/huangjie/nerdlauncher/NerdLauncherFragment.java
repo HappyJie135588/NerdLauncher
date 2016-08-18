6a7fe027d62954bd63d035397af159d479eb7e24
HuangJie <361125241@qq.com>
2016-08-18 10:10:37 +0800

    首次提交

---
+++ app/src/main/java/com/huangjie/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,30 @@
+package com.huangjie.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by HuangJie on 2016/8/17.
+ */
+public class NerdLauncherFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    public static NerdLauncherFragment newInstance(){
+        return new NerdLauncherFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_nerd_launcher,container,false);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.fragment_nerd_launcher_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
